e050617fc09f1bb29feffe34e030390fc82e3b84
Jingping Yin <yinjingping2022@gmail.com>
2025-04-28 10:24:11 +0800

    chore: prover type env

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -29,7 +29,9 @@ async fn main() -> eyre::Result<()> {
         std::env::set_var("RUST_LOG", "info");
     }
 
-    std::env::set_var("ZKM_PROVER", "local");
+    if std::env::var("ZKM_PROVER").is_err() {
+        std::env::set_var("ZKM_PROVER", "local");
+    }
 
     // Initialize the logger.
     tracing_subscriber::registry()
